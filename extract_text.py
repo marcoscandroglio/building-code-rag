784b94a4ceb355faa420166b3292db19d7ca33e7
marcoscandroglio <oilgordnacsocram@gmail.com>
2024-09-05 21:16:55 -0400

    vector database functions

--- extract_text.py
+++ extract_text.py
@@ -1,31 +1,35 @@
 import fitz
+import os
 
 
-def extract_text(pdf_path: str) -> list:
+def extract_text(pdf_dir: str) -> list:
     '''
     Function to divide the source PDF into text blocks
     according to its existing structure
     '''
 
-    document = fitz.open(pdf_path)
     block_list = []
+    for file in os.listdir(pdf_dir):
+        file_path = os.path.join(pdf_dir, file)
+        print(file_path)
+        document = fitz.open(file_path)
 
-    for page_num in range(document.page_count):
-        page = document.load_page(page_num)
-        blocks = page.get_text('blocks')
+        for page_num in range(document.page_count):
+            page = document.load_page(page_num)
+            blocks = page.get_text('blocks')
 
-        for block in blocks:
-            block_text = block[4].strip()
-            if block_text:
-                block_list.append(block_text)
+            for block in blocks:
+                block_text = block[4].strip()
+                if block_text:
+                    block_list.append(block_text)
 
     return block_list
 
 
 if __name__ == '__main__':
-    DIRECTORY = '2022BC/0_2022BC_Chapter01_AdministrationWBwm.pdf'
+    DIRECTORY = '2022BC/'
     pdf_blocks = extract_text(DIRECTORY)
 
-    for i, text_block in enumerate(pdf_blocks):
-        print(f"Block {i + 1}:\n{text_block}\n")
-        print("-" * 100)
+    # for i, text_block in enumerate(pdf_blocks):
+    #     print(f"Block {i + 1}:\n{text_block}\n")
+    #     print("-" * 100)
---
+++ vector_db.py
@@ -0,0 +1,20 @@
+import faiss
+import numpy as np
+
+def create_vector_index(embeddings, index_path='vector_index.faiss'):
+    embeddings_np = embeddings.cpu().detatch().numpy()
+    dimension = embeddings_np.shape[1]
+    index = faiss.IndexFlatL2(dimension)
+    index.add(embeddings_np)
+    faiss.write_index(index, index_path)
+    return index
+
+
+def load_vector_index(index_path='vector_index.faiss'):
+    return faiss.read_index(index_path)
+
+
+def search_vector_db(query, model, index, pdf_blocks, top_k=5):
+    query_embedding = model.encode([query], convert_to_tensor=True).cpu().numpy()
+    distances, indices = index.search(query_embedding, top_k)
+    return [pdf_blocks[i] for i in indices[0]]
